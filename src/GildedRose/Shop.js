d646f914009393c95ef23815da6e39d6bd86984f
Thierry Marianne <thierry@marianne.io>
2021-02-21 18:37:30 +0100

    Covered that sellIns are decreased for all items except for sulfuras

--- src/GildedRose/Shop.js
+++ src/GildedRose/Shop.js
@@ -33,9 +33,11 @@ class Shop {
           }
         }
       }
+
       if (this.items[i].name !== "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name !== "Aged Brie") {
           if (
--- tests/Integration/GildedRose.test.js
+++ tests/Integration/GildedRose.test.js
@@ -8,59 +8,70 @@ const backstagePasses = "Backstage passes to a TAFKAL80ETC concert";
 const conjuredManaCake = "Conjured Mana Cake";
 
 describe("Gilded Rose", function () {
-  it("should preserve the names of items when assessing their quality.", function () {
-    const items = [
-      new Item(dexterityVest, 10, 20),
-      new Item(agedBrie, 2, 0),
-      new Item(elixirOfTheMongose, 5, 7),
-      new Item(sulfurasHandOfRagnaros, 0, 80),
-      new Item(sulfurasHandOfRagnaros, -1, 80),
-      new Item(backstagePasses, 15, 20),
-      new Item(backstagePasses, 10, 49),
-      new Item(backstagePasses, 5, 49),
-      // this conjured item does not work properly yet
-      new Item(conjuredManaCake, 3, 6)
-    ];
+  it(
+    "should preserve the names of items when assessing their quality.",
+    function () {
+      const items = [
+        new Item(dexterityVest, 10, 20),
+        new Item(agedBrie, 2, 0),
+        new Item(elixirOfTheMongose, 5, 7),
+        new Item(sulfurasHandOfRagnaros, 0, 80),
+        new Item(sulfurasHandOfRagnaros, -1, 80),
+        new Item(backstagePasses, 15, 20),
+        new Item(backstagePasses, 10, 49),
+        new Item(backstagePasses, 5, 49),
+        // this conjured item does not work properly yet
+        new Item(conjuredManaCake, 3, 6)
+      ];
 
-    const gildedRose = new Shop(items);
-    const updatedItems = gildedRose.updateQuality();
+      const gildedRose = new Shop(items);
+      const updatedItems = gildedRose.updateQuality();
 
-    expect(updatedItems[0].name).toBe(dexterityVest);
-    expect(updatedItems[1].name).toBe(agedBrie);
-    expect(updatedItems[2].name).toBe(elixirOfTheMongose);
-    expect(updatedItems[3].name).toBe(sulfurasHandOfRagnaros);
-    expect(updatedItems[4].name).toBe(sulfurasHandOfRagnaros);
-    expect(updatedItems[5].name).toBe(backstagePasses);
-    expect(updatedItems[6].name).toBe(backstagePasses);
-    expect(updatedItems[7].name).toBe(backstagePasses);
-    expect(updatedItems[8].name).toBe(conjuredManaCake);
-  });
+      expect(updatedItems[0].name).toBe(dexterityVest);
+      expect(updatedItems[1].name).toBe(agedBrie);
+      expect(updatedItems[2].name).toBe(elixirOfTheMongose);
+      expect(updatedItems[3].name).toBe(sulfurasHandOfRagnaros);
+      expect(updatedItems[4].name).toBe(sulfurasHandOfRagnaros);
+      expect(updatedItems[5].name).toBe(backstagePasses);
+      expect(updatedItems[6].name).toBe(backstagePasses);
+      expect(updatedItems[7].name).toBe(backstagePasses);
+      expect(updatedItems[8].name).toBe(conjuredManaCake);
+    }
+  );
 
-  it("should preserve the sellIns of items when assessing their quality.", function () {
-    const items = [
-      new Item(dexterityVest, 10, 20),
-      new Item(agedBrie, 2, 0),
-      new Item(elixirOfTheMongose, 5, 7),
-      new Item(sulfurasHandOfRagnaros, 0, 80),
-      new Item(sulfurasHandOfRagnaros, -1, 80),
-      new Item(backstagePasses, 15, 20),
-      new Item(backstagePasses, 10, 49),
-      new Item(backstagePasses, 5, 49),
-      // this conjured item does not work properly yet
-      new Item(conjuredManaCake, 3, 6)
-    ];
+  it(
+    "should decrease the sellIns of items different from sulfuras when assessing their quality.",
+    function () {
+      const sellIns = [
+        10,
+        2,
+        5,
+        15,
+        10,
+        5,
+        3
+      ];
 
-    const gildedRose = new Shop(items);
-    const updatedItems = gildedRose.updateQuality();
+      const items = [
+        new Item(dexterityVest, 10, 20),
+        new Item(agedBrie, 2, 0),
+        new Item(elixirOfTheMongose, 5, 7),
+        new Item(backstagePasses, 15, 20),
+        new Item(backstagePasses, 10, 49),
+        new Item(backstagePasses, 5, 49),
+        new Item(conjuredManaCake, 3, 6)
+      ];
 
-    expect(updatedItems[0].sellIn).toBe(updatedItems[0].sellIn);
-    expect(updatedItems[1].sellIn).toBe(updatedItems[1].sellIn);
-    expect(updatedItems[2].sellIn).toBe(updatedItems[2].sellIn);
-    expect(updatedItems[3].sellIn).toBe(updatedItems[3].sellIn);
-    expect(updatedItems[4].sellIn).toBe(updatedItems[4].sellIn);
-    expect(updatedItems[5].sellIn).toBe(updatedItems[5].sellIn);
-    expect(updatedItems[6].sellIn).toBe(updatedItems[6].sellIn);
-    expect(updatedItems[7].sellIn).toBe(updatedItems[7].sellIn);
-    expect(updatedItems[8].sellIn).toBe(updatedItems[8].sellIn);
-  });
+      const gildedRose = new Shop(items);
+      const updatedItems = gildedRose.updateQuality();
+
+      expect(updatedItems[0].sellIn).toBe(sellIns[0] - 1);
+      expect(updatedItems[1].sellIn).toBe(sellIns[1] - 1);
+      expect(updatedItems[2].sellIn).toBe(sellIns[2] - 1);
+      expect(updatedItems[3].sellIn).toBe(sellIns[3] - 1);
+      expect(updatedItems[4].sellIn).toBe(sellIns[4] - 1);
+      expect(updatedItems[5].sellIn).toBe(sellIns[5] - 1);
+      expect(updatedItems[6].sellIn).toBe(sellIns[6] - 1);
+    }
+  );
 });
