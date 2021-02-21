f4723449a0b61c6e0efd562b64596008fbf01f58
Thierry Marianne <thierry@marianne.io>
2021-02-21 19:21:51 +0100

    Formatted Item module

--- src/GildedRose/Item.js
+++ src/GildedRose/Item.js
@@ -1,14 +1,13 @@
-
 class Item {
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+    constructor(name, sellIn, quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
 
-  updateQuality() {
-    // TODO
-  }
+    updateQuality() {
+        // TODO
+    }
 }
 
 module.exports = Item;
