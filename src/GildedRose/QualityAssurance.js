65d1e0939b459ed2c0f4bf801ddabaffbd38d3b7
Thierry Marianne <thierry@marianne.io>
2021-02-21 23:31:04 +0100

    Moved static functions Quality Assurance class is responsible for around

--- src/GildedRose/QualityAssurance.js
+++ src/GildedRose/QualityAssurance.js
@@ -5,6 +5,25 @@ class QualityAssurance {
     static isOfAcceptableQuality(item) {
         return !isNaN(item.quality) && item.quality >= 0;
     }
+
+    static isNotOfStableQuality(shoppingArticle) {
+        const isOfStableQuality = typeof QualityAssurance.STABLE_QUALITY_OVER_TIME[shoppingArticle.name()] !== 'undefined';
+
+        if (isOfStableQuality) {
+            return false
+        }
+
+        return true;
+    }
+
+    static getStableQuality(shoppingArticle) {
+        if (QualityAssurance.isNotOfStableQuality(shoppingArticle)) {
+            throw 'This item is not of stable quality';
+        }
+
+        return QualityAssurance.STABLE_QUALITY_OVER_TIME[shoppingArticle.name()];
+    }
+
 }
 
 QualityAssurance.MAX_QUALITY = 50;
--- src/GildedRose/Shop/ShoppingArticle.js
+++ src/GildedRose/Shop/ShoppingArticle.js
@@ -27,24 +27,6 @@ class ShoppingArticle {
         return this.item.quality;
     }
 
-    isNotOfStableQuality() {
-        const isOfStableQuality = typeof QualityAssurance.STABLE_QUALITY_OVER_TIME[this.name()] !== 'undefined';
-
-        if (isOfStableQuality) {
-            return false
-        }
-
-        return true;
-    }
-
-    getStableQuality() {
-        if (this.isNotOfStableQuality()) {
-            throw 'This item is not of stable quality';
-        }
-
-        return QualityAssurance.STABLE_QUALITY_OVER_TIME[this.name()];
-    }
-
     isNotInShopCatalogue() {
         return ! Object.values(ItemName).includes(this.name());
     }
@@ -56,15 +38,15 @@ class ShoppingArticle {
             throw `Can not find item in shop by name: "${shoppingArticle.name()}"`
         }
 
-        if (shoppingArticle.isNotOfStableQuality()) {
+        if (QualityAssurance.isNotOfStableQuality(shoppingArticle)) {
             return shoppingArticle;
         }
 
-        if (shoppingArticle.getStableQuality() !== shoppingArticle.quality()) {
+        if (QualityAssurance.getStableQuality(shoppingArticle) !== shoppingArticle.quality()) {
             throw [
                 `"${ItemName.sulfurasHandOfRagnaros}" items are legendary, `,
                 'hence their quality have to be compliant with a standard of ',
-                `${shoppingArticle.getStableQuality()}.`
+                `${QualityAssurance.getStableQuality(shoppingArticle)}.`
             ].join('')
         }
 
