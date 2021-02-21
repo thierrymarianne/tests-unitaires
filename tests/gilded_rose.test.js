929f2044c8d4f6782e9434d946025bd26ab0170e
Thierry Marianne <thierrymarianne@users.noreply.github.com>
2021-02-21 09:42:26 +0100

    Added project files

---
+++ tests/gilded_rose.test.js
@@ -0,0 +1,22 @@
+const { Shop, Item } = require("../src/gilded_rose");
+
+describe("Gilded Rose", function () {
+  it("should foo", function () {
+    const items = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      // this conjured item does not work properly yet
+      new Item("Conjured Mana Cake", 3, 6)
+    ];
+
+    const gildedRose = new Shop(items);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].name).toBe("fixme");
+  });
+});
