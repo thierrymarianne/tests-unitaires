3fb52b8b79b1d717df796f7ab3d92f60203cb503
Thierry Marianne <thierry@marianne.io>
2021-02-22 22:12:58 +0100

    Deprecated original method assessing the quality of items

--- src/GildedRose/Shop/Shop.js
+++ src/GildedRose/Shop/Shop.js
@@ -79,65 +79,6 @@ class Shop {
         return this.items;
     }
 
-    assessQualityOfItems() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (
-                this.items[i].name !== ItemName.agedBrie &&
-                this.items[i].name !== ItemName.backstagePasses
-            ) {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name !== ItemName.sulfurasHandOfRagnaros) {
-                        this.items[i].quality = this.items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (this.items[i].quality < QualityAssurance.MAX_QUALITY) {
-                    this.items[i].quality = this.items[i].quality + 1;
-                    if (
-                        this.items[i].name === ItemName.backstagePasses
-                    ) {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < QualityAssurance.MAX_QUALITY) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < QualityAssurance.MAX_QUALITY) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (this.items[i].name !== ItemName.sulfurasHandOfRagnaros) {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name !== ItemName.agedBrie) {
-                    if (
-                        this.items[i].name !== ItemName.backstagePasses
-                    ) {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name !== ItemName.sulfurasHandOfRagnaros) {
-                                this.items[i].quality = this.items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = 0;
-                    }
-                } else {
-                    if (this.items[i].quality < QualityAssurance.MAX_QUALITY) {
-                        this.items[i].quality = this.items[i].quality + 1;
-                    }
-                }
-            }
-        }
-
-        return this.items;
-    }
-
     updateQuality() {
         return this.assessQualityOfShoppingArticlesInCart()
     }
